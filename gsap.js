e14752458e6ea1b3ea33df144e1c19e6cf1cd6c6
Jason Nutt <jnutt367@gmail.com>
2021-04-22 19:12:01 -0400

    commit new volcano animation

--- gsap.js
+++ gsap.js
@@ -2,3 +2,5 @@ gsap.from("#cloud", { duration: .5, scale: 0.9, ease: "elastic", y: -10, repeat:
 gsap.from("#cloud2", { duration: 2, scale: 1.1, ease: "back", y: -10, repeat: -1 })
 gsap.to("#red-bubble", { duration: 5, scale: 0.6, ease: "bounce", repeat: -1 })
 gsap.from("#red-bubble-small", { duration: 5, scale: 0.6, ease: "bounce", repeat: -1 })
+
+gsap.to("#volcano", { x: 5, y: -2, duration: .01, ease: "rough", repeat: -1 })
