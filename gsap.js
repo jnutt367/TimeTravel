fc7d7b2716b4b8629fae08785eb83ecca55fe823
Jason Nutt <jnutt367@gmail.com>
2021-04-22 19:57:04 -0400

    commit for new bubbles at bottom of volcano animations

--- gsap.js
+++ gsap.js
@@ -1,6 +1,8 @@
-gsap.from("#cloud", { duration: .5, scale: 0.9, ease: "elastic", y: -10, repeat: -1 })
+gsap.to("#cloud", { duration: 1.9, y: 100, scale: 1.1, ease: "elastic", y: -10, repeat: -1 })
 gsap.from("#cloud2", { duration: 2, scale: 1.1, ease: "back", y: -10, repeat: -1 })
 gsap.to("#red-bubble", { duration: 5, scale: 0.6, ease: "bounce", repeat: -1 })
 gsap.from("#red-bubble-small", { duration: 5, scale: 0.6, ease: "bounce", repeat: -1 })
 
 gsap.to("#volcano", { x: 5, y: -2, duration: .01, ease: "rough", repeat: -1 })
+gsap.to("#second-red-bubble-small", { duration: 1.7, scale: 0.6, ease: "bounce", repeat: -1 })
+gsap.to("#second-red-bubble", { duration: 1.1, scale: 1.4, ease: "bounce", repeat: -1 })
