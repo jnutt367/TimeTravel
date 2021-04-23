77093e3da74dc1aab62bc654fc7f02da1f2515eb
Jason Nutt <jnutt367@gmail.com>
2021-04-23 13:32:40 -0400

    commit new terry move

--- gsap.js
+++ gsap.js
@@ -1,13 +1,29 @@
-var tl = gsap.timeline()
+gsap.registerPlugin(ScrollTrigger)
+    //
+    //var tl = gsap.timeline()
 
-gsap.to("#cloud", { duration: 1.9, y: 100, scale: 1.1, ease: "elastic", y: -10, repeat: -1 })
-gsap.from("#cloud2", { duration: 2, scale: 1.1, ease: "back", y: -10, repeat: -1 })
-gsap.to("#red-bubble", { duration: 5, scale: 0.6, ease: "bounce", repeat: -1 })
-gsap.from("#red-bubble-small", { duration: 5, scale: 0.6, ease: "bounce", repeat: -1 })
+//    gsap.to("#cloud", { duration: 1.9, y: 100, scale: 1.1, ease: "elastic", y: -10, repeat: -1 })
+//    gsap.from("#cloud2", { duration: 2, scale: 1.1, ease: "back", y: -10, repeat: -1 })
+//    gsap.to("#red-bubble", { duration: 5, scale: 0.6, ease: "bounce", repeat: -1 })
+//    gsap.from("#red-bubble-small", { duration: 5, scale: 0.6, ease: "bounce", repeat: -1 })
 
-gsap.to("#volcano", { x: 5, y: -2, duration: .01, ease: "rough", repeat: -1 })
-gsap.to("#second-red-bubble-small", { duration: 1.7, scale: 0.6, ease: "bounce", repeat: -1 })
-gsap.to("#second-red-bubble", { duration: 1.1, scale: 1.4, ease: "bounce", repeat: -1 })
-gsap.to("#first-tree", { x: 2, duration: .1, ease: "rough", repeat: -1 })
-gsap.fromTo("#terry", { x: 100 }, { duration: 1, y: -800 })
-gsap.to("#terry", { duration: 6, x: 400 })
+//    gsap.to("#volcano", { x: 5, y: -2, duration: .01, ease: "rough", repeat: -1 })
+//    gsap.to("#second-red-bubble-small", { duration: 1.7, scale: 0.6, ease: "bounce", repeat: -1 })
+//    gsap.to("#second-red-bubble", { duration: 1.1, scale: 1.4, ease: "bounce", repeat: -1 })
+//    gsap.to("#first-tree", { x: 2, duration: .1, ease: "rough", repeat: -1 })
+//    gsap.fromTo("#terry", { x: 100 }, { duration: 1, y: -800 })
+//    gsap.to("#terry", { duration: 6, x: 400 })
+//    gsap.to("#terry", { x: 100, y: -100, })
+gsap.to("#terry", {
+    duration: 10,
+    x: 400,
+    y: -650,
+    ease: "elastic",
+    scrollTrigger: {
+        trigger: "#volcano",
+        markers: true,
+        start: "top 50%",
+        end: "end 40%",
+        toggleActions: "restart complete reverse reverse"
+    }
+})
