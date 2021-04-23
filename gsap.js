10bb9b39b6f37884050ea6cf3662a71ddadc4785
Jason Nutt <jnutt367@gmail.com>
2021-04-22 21:10:52 -0400

    commit first tree

--- gsap.js
+++ gsap.js
@@ -6,3 +6,4 @@ gsap.from("#red-bubble-small", { duration: 5, scale: 0.6, ease: "bounce", repeat
 gsap.to("#volcano", { x: 5, y: -2, duration: .01, ease: "rough", repeat: -1 })
 gsap.to("#second-red-bubble-small", { duration: 1.7, scale: 0.6, ease: "bounce", repeat: -1 })
 gsap.to("#second-red-bubble", { duration: 1.1, scale: 1.4, ease: "bounce", repeat: -1 })
+gsap.to("#first-tree", { x: 2, duration: .1, ease: "rough", repeat: -1 })
